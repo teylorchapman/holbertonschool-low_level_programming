5df2d9ed79ea0105d28a245c00ca245d4f674e7c
teylorchapman <teylor.chapman2@gmail.com>
2022-09-19 10:51:57 -0700

    5-print_numbers.c

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+/**
+* main - Prints all signle digit numbers starting at 0.
+*
+* Return: Always 0
+*/
+int main(void)
+{
+int numero;
+for (numero = 0; numero < 10; numero++)
+printf("%d", numero);
+return (0);
+}
