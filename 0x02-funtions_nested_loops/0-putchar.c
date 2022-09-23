938b74e835dee0210e9d7f1a1dc8b80226628290
teylorchapman <teylor.chapman2@gmail.com>
2022-09-23 07:35:37 -0700

    putchar with a return of 0

---
+++ 0x02-funtions_nested_loops/0-putchar.c
@@ -0,0 +1,21 @@
+#include main.h
+/**
+* main - Write a program that prints _putchar
+* followed by a new line
+*
+* return - 0
+*/
+int main(void)
+{
+putchar ('_');
+putchar ('p');
+putchar ('u');
+putchar ('t');
+putchar ('c');
+putchar ('h');
+putchar ('a');
+putchar ('r');
+putchar ('\n');
+
+return (0);
+}
---
+++ 0x02-funtions_nested_loops/README.md
@@ -0,0 +1,1 @@
+Learning how to make nested loops in C
