88c8839f66cc5782ba004bb4c89e6b9202e16d00
teylorchapman <teylor.chapman2@gmail.com>
2022-09-23 16:29:12 -0700

    lowercase alphabet

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,17 @@
+#include <main.h>
+/**
+*main - prints alphabet a-z
+*
+*return = always 0.
+*/
+void print_alphabet(void)
+{
+char alpha;
+alpha = 'a';
+while (alpha <= 'z')
+{
+_printchar(alpha);
+alpha++;
+}
+_putchar_('\n');
+}
