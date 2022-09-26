765153ac7fb7e4804518e0acd2d45795920939b6
teylorchapman <teylor.chapman2@gmail.com>
2022-09-26 08:03:55 -0700

    printing numbers 1 to 9

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+* print_numbers - prints 0-9 followed by new line
+* Return: Void. no return.
+*/
+void print_numbers(void)
+{
+int i = 0;
+for (i = 0; i < 10; i++)
+_putchar(i + '0');
+_putchar('\n');
+}
