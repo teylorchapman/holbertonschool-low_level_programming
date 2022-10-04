d864930afa5cea0fe1e46c42240fa93074c3e185
teylorchapman <teylor.chapman2@gmail.com>
2022-10-04 09:45:30 -0700

    strcmp

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -7,15 +7,11 @@
 *more than 0 if s1 is greater than s2
 */
 int *_strcmp(char *s1, char *s2)
+{int i;
+for (i = 0; s1[i] != '\0' && s2[i] != '\0'; i++)
 {
-while (*s1 == *s2)
-{
-if (*s1 == '\0')
-{
+if (s1[i] != s2[i])
+return (s1[i] - s2[i]);
+}
 return (0);
-}
-s1++;
-s2++;
-}
-return (*s1 - *s2);
 }
