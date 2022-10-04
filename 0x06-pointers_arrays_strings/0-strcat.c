4bef073aa40733c4d2b8d6f2904681c446bb3cab
teylorchapman <teylor.chapman2@gmail.com>
2022-10-04 09:04:24 -0700

    strcat

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,18 @@
+#include "holberton.h"
+/**
+*_strcat - a function that concatenates two strings
+*@dest: copy to
+*@src: copy from
+*return: two concat strings
+*/
+char *_strcat(char *dest, char *src)
+{
+int i, n;
+for (i = 0; dest[i] != '\0'; i++)
+{
+}
+for (n = 0; (dest[i + n] = *src++) != '\0'; n++)
+{
+}
+return (dest);
+}
