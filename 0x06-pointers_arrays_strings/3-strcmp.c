09bf849e7a39c48c7eb8874bed0c07b6a822e28a
teylorchapman <teylor.chapman2@gmail.com>
2022-10-04 17:26:50 -0700

    strcmp

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -1,13 +1,13 @@
 #include "holberton.h"
 /**
-*_strcmp - compares two strings
-*@s1: first string to compare
-*@s2: second string to compare
-*return: less than 0 if s1 is less than s2, 0 if they're
-*more than 0 if s1 is greater than s2
+* _strcmp - a function that compares two strings
+* @s1: input one
+* @s2: input two
+* Return: Always 0 (Success)
 */
-int *_strcmp(char *s1, char *s2)
-{int i;
+int _strcmp(char *s1, char *s2)
+{
+int i;
 for (i = 0; s1[i] != '\0' && s2[i] != '\0'; i++)
 {
 if (s1[i] != s2[i])
