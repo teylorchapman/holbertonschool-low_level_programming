ff85cd5a4ed93e56bae3b73981a241050db12eb1
teylorchapman <teylor.chapman2@gmail.com>
2022-10-10 20:12:45 -0700

    puts recursion

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+*_puts_recursion - entry point
+*@s: input
+*return: always 0
+*/
+void _puts_recursion(char *s)
+{
+if (*s == '\0')
+_putchar('\n')
+else
+{
+_putchar(*s);
+_puts_recursion(s + 1);
+}
+}
