fad49c32f5ba72f2b19871515f3915cc64642985
teylorchapman <teylor.chapman2@gmail.com>
2022-10-25 07:53:58 -0700

    dog.h

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -6,12 +6,14 @@
 *@age: age
 *@owner: owner
 */
+
 typedef struct dog
 {
 char *name;
 float age;
 char *owner;
 }
+
 dog_t;
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
