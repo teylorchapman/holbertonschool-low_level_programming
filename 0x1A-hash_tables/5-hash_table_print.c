2fa7d0dae313eb641796967561e294bf4ad94e5a
teylorchapman <teylor.chapman2@gmail.com>
2022-11-15 05:28:05 -0800

    5-hash_table_print.c

--- 0x1A-hash_tables/5-hash_table_print.c
+++ 0x1A-hash_tables/5-hash_table_print.c
@@ -3,33 +3,34 @@
 /**
 * hash_table_print - prints a hash table
 * @ht: the hash table to print
-* return: nothing
+* return: void
 */
 
 void hash_table_print(const hash_table_t *ht)
 {
-hash_node_t *node = NULL;
-unsigned long int i = 0;
-short pr_commas = 0;
+unsigned long int i, flag = 0;
+hash_node_t *current;
 
 if (ht == NULL)
 return;
-
-printf("{");
-
-for (; i < ht->size; i++)
+putchar('{');
+for (i = 0; i < ht->size; i++)
 {
-node = ht->array[i];
-
-while (node)
+current = ht->array[i];
+if (current != NULL)
 {
-if (pr_commas == 1)
-printf(" , ");
-
-pr_commas = 1;
-printf("'%s' : '%s'", (char *) node->key, (char *) node->value);
-node = node->next;
+while (current != NULL)
+{
+if (flag == 1)
+{
+printf(", ");
+}
+printf("'%s': '%s'", current->key, current->value);
+flag = 1;
+current = current->next;
 }
 }
-printf("}\n");
+}
+putchar('}');
+putchar('\n');
 }
