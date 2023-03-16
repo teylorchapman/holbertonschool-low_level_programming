d4eb2165150536abd994065bebc9d5544f165d9a
teylorchapman <5535@holbertonstudent.com>
2023-03-16 12:05:24 -0700

    pRiNt In BiNaRy

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -1,9 +1,9 @@
 #include "main.h"
 
 /**
- * print_binary - prints the binary representation of a number
- * @n: number to be printed
- * Return: void
+* print_binary - prints the binary representation of a number
+* @n: number to be printed
+* Return: void
 */
 
 void print_binary(unsigned long int n)
@@ -11,5 +11,5 @@ void print_binary(unsigned long int n)
 if (n > 1)
 print_binary(n >> 1);
 
-_putchar((n & 1) + '0')
+_putchar((n & 1) + '0');
 }
