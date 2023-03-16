9a1d8afb4afb4970c8c115ab1f38e1de95e4b9e1
teylorchapman <5535@holbertonstudent.com>
2023-03-16 13:37:45 -0700

    idkwtfigo

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -13,16 +13,15 @@ int append_text_to_file(const char *fn, char *tc)
 int o, w, len = 0;
 if (fn == NULL)
 return (-1);
-
 if (tc != NULL)
 {
-for (len = 0, tc[len];)
+for (len = 0; tc[len];)
 len++;
 }
 o = open(fn, O_WRONLY | O_APPEND);
 w = write(o, tc, len);
 if (o == -1 || w == -1)
 return (-1);
-close(0);
+close(o);
 return (1);
 }
