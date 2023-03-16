d6b5e0709eb9197c758de39e2e2c234da83f9f59
teylorchapman <5535@holbertonstudent.com>
2023-03-16 11:22:44 -0700

    :) if only you could see my eye twitch

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -10,46 +10,46 @@
 
 listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 {
-listint_t *nn = NULL;
-listint_t *pn = NULL;
+listint_t *new_node = NULL;
+listint_t *previous_node = NULL;
 unsigned int i = 0;
 
-nn = malloc(sizeof(listint_t));
-if (nn == NULL || idx > listint_len(*head))
+new_node = malloc(sizeof(listint_t));
+if (new_node == NULL || idx > listint_len(*head))
 {
-free(nn);
+free(new_node);
 return (NULL);
 }
-nn->n = n;
-nn->next = NULL;
+new_node->n = n;
+new_node->next = NULL;
 while (head != NULL)
 {
 if (i == idx)
 {
 if (i == 0)
 {
-nn->next = *head;
-*head = nn;
-return (nn);
+new_node->next = *head;
+*head = new_node;
+return (new_node);
 }
-nn->next = pn->next;
-pn->next = nn;
-return (nn);
+new_node->next = previous_node->next;
+previous_node->next = new_node;
+return (new_node);
 }
 else if ((i + 1) == idx)
-pn = *head;
-head = ((*head)->next);
+previous_node = *head;
+head = &((*head)->next);
 i++;
 }
 return (NULL);
 }
 
 /**
- * listint_len - counts the number of nodes in a linked list
- * @h: of the list
- * Return: the number of elements
+* listint_len - counts the number of nodes in a linked list
+* @h: head of the list
+*
+* Return: the number of elements
 */
-
 size_t listint_len(const listint_t *h)
 {
 const listint_t *cursor = h;
