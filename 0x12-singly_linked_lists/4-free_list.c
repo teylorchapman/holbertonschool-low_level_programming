dd2e32321dc03190b01cd994896d180dd6cc5b01
teylorchapman <5535@holbertonstudent.com>
2023-03-16 09:39:58 -0700

    aw yeah

---
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -0,0 +1,20 @@
+#include "lists.h"
+
+/**
+ * free_list - frees a list
+ * @head: a pointer to the list head
+ * Return: void
+*/
+
+void free_list(list_t *head)
+{
+slist_t *tmp;
+
+while (head)
+{
+tmp = head->next;
+free(head->str);
+free(head);
+head = tmp;
+}
+}
