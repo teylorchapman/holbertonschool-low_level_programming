63957d65ccda73cfd115b6b41839f3cfe8e4bc4c
teylorchapman <5535@holbertonstudent.com>
2023-03-16 10:26:42 -0700

    will this work?????

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -9,16 +9,16 @@
 void free_listint2(listint_t **head)
 {
 listint_t *cursor;
-listint_t **temp = head;
 
-if (temp != NULL)
+if (!(head))
 {
-while (*head != NULL)
+return;
+}
+while (*head)
 {
-cursor = *head;
-free(cursor);
-*head = (*head)->next;
+cursor = (*head)->next;
+free(*head);
+*head = cursor;
 }
-*temp = NULL;
-}
+head = NULL;
 }
