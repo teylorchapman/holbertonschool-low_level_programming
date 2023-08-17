6a1dc07dddd87460e6cfb03f0f360e6caf4bbbab
Teylor Chapman <5535@holbertonstudents.com>
2023-08-17 14:23:44 -0500

    betty knows no love

--- search_algorithms/0-linear.c
+++ search_algorithms/0-linear.c
@@ -5,22 +5,22 @@
  * @array: a pointer to the first element of the arry to search in
  * @size: number of elements in array
  * @value: value to search for
- * 
+ *
  * Return: the first index where value is located.
 */
 
 int linear_search(int *array, size_t size, int value)
 {
-    int i;
+int i;
 
-    if(!array)
-        return (-1);
+if(!array)
+return (-1);
     
-    for (i = 0; i < (int)size; i++)
-    {
-        printf("Value checked array[%d] = [%d]\n", i, array[i]);
-        if (array[i] == value)
-            return (i);
-    }
-    return (-1);
+for (i = 0; i < (int)size; i++)
+{
+printf("Value checked array[%d] = [%d]\n", i, array[i]);
+if (array[i] == value)
+return (i);
+}
+return (-1);
 }
