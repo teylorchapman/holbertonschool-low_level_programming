398c5f19fe6e878f18cf6e8f9525ffaaa830ccf5
Teylor Chapman <5535@holbertonstudents.com>
2023-08-17 14:36:49 -0500

    binary

---
+++ search_algorithms/1-binary.c
@@ -0,0 +1,35 @@
+#include "search_algos.h"
+
+/**
+ * binary_search - searches for a value in a sorted array of integers
+ * @array: a pointer to the first element of the array to search in
+ * @size: number of elements in array
+ * @value: value to search for
+ *
+ * Return: the index where value is located
+*/
+int binary_search(int *array, size_t size, int value)
+{
+int left = 0;
+int right = (int)size - 1;
+int mid;
+int i;
+
+while (left <= right)
+{
+printf("Searching in array: ");
+for (i = left; i < right; i++)
+printf("%d\n", array[i]);
+printf("%d\n", array[i]);
+
+mid = (left + right) / 2;
+
+if (array[mid] < value)
+left = mid + 1;
+else if (array[mid] > value)
+right = mid - 1;
+else
+return (mid);
+}
+return (-1);
+}
